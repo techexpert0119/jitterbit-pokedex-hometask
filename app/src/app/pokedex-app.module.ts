9b9fd8f4c0dcb469b1afa41bebf4d76321f0413b
Ian Harding <ianharding728@gmail.com>
2024-08-08 09:21:13 +0100

    feat: install angular material

--- app/src/app/pokedex-app.module.ts
+++ app/src/app/pokedex-app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Add this
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './pokedex-app-routing.module';
 import { PokedexAppComponent } from './pokedex-app.component';
 import { PokemonService } from './services/pokemon.service';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
-  providers: [PokemonService],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
+  providers: [PokemonService, provideAnimationsAsync()],
   declarations: [PokedexAppComponent, PokemonListComponent],
   bootstrap: [PokedexAppComponent],
 })
